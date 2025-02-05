0628fd422e5b642116d8aedc84cdcfd845bfe9a3
dhara-greycube <dhara@greycube.in>
2025-02-05 15:58:09 +0530

    feat:calculate no of days delay days in Return from vacation

--- doitc/doitc/doctype/return_from_vacation_dt/return_from_vacation_dt.js
+++ doitc/doitc/doctype/return_from_vacation_dt/return_from_vacation_dt.js
@@ -2,7 +2,27 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Return From Vacation DT', {
-	// refresh: function(frm) {
-
-	// }
+	
+	return_date : function(frm){
+		let start_date = frm.doc.return_date;
+		let end_date = frm.doc.return_to_work_date;
+		let date_diff = get_diff(start_date, end_date);
+		frm.set_value("no_of_delay", date_diff + 1)
+	},
+	return_to_work_date : function(frm){
+		let end_date = frm.doc.return_to_work_date;
+		let start_date = frm.doc.return_date;
+		let date_diff = get_diff(start_date, end_date);
+		frm.set_value("no_of_delay", date_diff + 1)
+	}
 });
+
+function get_diff(start_date,end_date) {
+	if(start_date > end_date){
+		frappe.throw(__("The date of return cannot be later than the date of delay"));
+	}
+	if(start_date && end_date){
+		let diff = frappe.datetime.get_day_diff(end_date, start_date);
+		return diff;
+	}
+}
