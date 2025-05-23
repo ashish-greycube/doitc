7cf83e5d8626a706bd420b35e0d1e9c9450ec97d
Sakshi-Greycube <sakshi@greycube.in>
2025-05-23 18:01:03 +0530

    feat: create new doctype for custody & new fields in serial no & item

--- doitc/migrations.py
+++ doitc/migrations.py
@@ -131,6 +131,63 @@ def create_custom_fields_in_core_doctype():
                 'is_custom_field' : 1,
                 'insert_after' : 'delivery_date',
             }
+        ],
+
+        "Serial No" : [
+            {
+                'fieldname' : 'custom_custody_details_section',
+                'fieldtype' : 'Section Break',
+                'label' : 'Custody Details',
+                'is_system_generated' : 0,
+                'is_custom_field' : 1,
+                'insert_after' : 'sales_order',
+            },
+            {
+                'fieldname' : 'custom_custody_with',
+                'fieldtype' : 'Link',
+                'options' : 'Employee',
+                'label' : 'Custody With',
+                'is_system_generated' : 0,
+                'is_custom_field' : 1,
+                'insert_after' : 'custom_custody_details_section',
+            },
+            {
+                'fieldname' : 'custom_custody_with_name',
+                'fieldtype' : 'Data',
+                'label' : 'Custody With Name',
+                'is_system_generated' : 0,
+                'is_custom_field' : 1,
+                'insert_after' : 'custom_custody_with',
+                'read_only' : 1,
+                'fetch_from' : 'custom_custody_with.employee_name',
+            },
+            {
+                'fieldname' : 'custom_custody_details_column_break',
+                'fieldtype' : 'Column Break',
+                'is_system_generated' : 0,
+                'is_custom_field' : 1,
+                'insert_after' : 'custom_custody_with_name',
+            },
+            {
+                'fieldname' : 'custom_item_condition',
+                'fieldtype' : 'Select',
+                'options' : '\nNew\nUsed',
+                'label' : 'Item Condition',
+                'is_system_generated' : 0,
+                'is_custom_field' : 1,
+                'insert_after' : 'custom_custody_details_column_break',
+            }
+        ],
+
+        "Item" : [
+            {
+                'fieldname' : 'custom_is_custody_item',
+                'fieldtype' : 'Check',
+                'label' : 'Is Custody Item',
+                'is_system_generated' : 0,
+                'is_custom_field' : 1,
+                'insert_after' : 'include_item_in_manufacturing',
+            }
         ]
     }
     print("Creating custom fields for app Doitc:")
